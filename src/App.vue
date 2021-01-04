46021ac3e5e56b98ccfb3f82970d703b9a701354
michael beloff <michael.a.beloff@gmail.com>
2021-01-04 14:09:44 +1000

    init

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="Vue logo" class="mx-auto" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite + Tailwindcss" />
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <button class="rounded-lg px-5 py-3 my-4 text-white bg-green-500 active:bg-green-700 focus:outline-none" @click="count++">count is: {{ count }}</button>
+  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
